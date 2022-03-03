c2e603ff47c8f39276a3fe00ea72cc82081d5fd9
Clement <clement.boulnager@efrei.net>
2022-03-03 12:55:59 +0100

    Use local model

--- web/predict.py
+++ web/predict.py
@@ -1,5 +1,7 @@
-from detoxify import Detoxify
 from googletrans import Translator
+import torch
+detoxify = torch.load('./web/model/detoxify.pt')
+model = detoxify('original')
 
 
 def checking_language(sentence):
@@ -14,4 +16,4 @@ def predict(sentence):
     if checking_language(sentence) == 0:
         raise Exception("The text language used must be English")
     else:
-        return Detoxify('original').predict(sentence)
+        return model.predict(sentence)
